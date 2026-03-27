99fb04bffa8fa52a866b6ef4a8cf15b0e628ee7b
Michal Pielka <mpielka726@gmail.com>
2026-03-27 23:52:47 +0100

    feat(core): add create_gist() function

--- envelop-core/src/github.rs
+++ envelop-core/src/github.rs
@@ -1,4 +1,6 @@
+use reqwest::blocking::Client;
 use serde::Deserialize;
+use serde_json::json;
 
 const BASE_URL: &str = "https://github.com";
 const API_URL: &str = "https://api.github.com";
@@ -22,4 +24,30 @@ pub fn fetch_public_keys(username: &str) -> Result<Vec<String>, Box<dyn std::err
 pub struct Gist {
     id: String,
     html_url: String,
+}
+
+// TODO: error handling
+pub fn create_gist(
+    content: &str,
+    recipient: &str,
+    token: &str,
+) -> Result<Gist, Box<dyn std::error::Error>> {
+    let create_gist_url = format!("{API_URL}/gists");
+
+    let client = Client::new();
+    let body = json!({
+        "description": format!("Envelop for {recipient}"),
+        "public": false,
+        "files": {"envelop.age": {"content": content}},
+    });
+    let resp = client
+        .post(create_gist_url)
+        .json(&body)
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {token}"))
+        .header("X-GitHub-Api-Version", "2026-03-10")
+        .send()?
+        .error_for_status()?;
+
+    Ok(resp.json()?)
 }
