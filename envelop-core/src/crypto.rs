8009b8fac373b08e93fee847f2bc8c408b3c62c2
Michal Pielka <mpielka726@gmail.com>
2026-03-27 21:04:02 +0100

    feat(core): add encrypt() function

--- envelop-core/src/crypto.rs
+++ envelop-core/src/crypto.rs
@@ -1,14 +1,35 @@
-use age::ssh::{ParseRecipientKeyError, Recipient};
-pub fn encrypt(plaintext: &[u8], public_keys: &[&str]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+use std::io::Write;
+
+use age::ssh::{ParseRecipientKeyError, Recipient as SshRecipient};
+use age::{Encryptor, Recipient};
+
+pub fn encrypt(
+    plaintext: &[u8],
+    public_keys: &[&str],
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let recipients = parse_recipients(public_keys).map_err(|_e| "TODO: Custom error")?;
+    let recipients = recipients.iter().map(|r| r as &dyn Recipient);
+
+    let encryptor = Encryptor::with_recipients(recipients)?;
+    let mut ciphertext = Vec::new();
+
+    let mut writer = encryptor.wrap_output(&mut ciphertext)?;
+    writer.write_all(plaintext)?;
+    writer.finish()?;
+
+    Ok(ciphertext)
+}
+
+pub fn decrypt(
+    ciphertext: &[u8],
+    private_key: &str,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     Ok(vec![])
 }
 
-pub fn decrypt(ciphertext: &[u8], private_key: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    Ok(vec![])
-}
-fn parse_recipients(public_keys: &[&str]) -> Result<Vec<Recipient>, ParseRecipientKeyError> {
+fn parse_recipients(public_keys: &[&str]) -> Result<Vec<SshRecipient>, ParseRecipientKeyError> {
     public_keys
         .iter()
-        .map(|&key| key.parse::<Recipient>())
+        .map(|&key| key.parse::<SshRecipient>())
         .collect()
 }
