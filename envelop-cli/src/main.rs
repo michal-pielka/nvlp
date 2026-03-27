3194a3ad628f771199b8f9b16cabcd65e5359217
Michal Pielka <mpielka726@gmail.com>
2026-03-28 00:17:25 +0100

    feat(cli): handle_send_command() blueprint

--- envelop-cli/src/main.rs
+++ envelop-cli/src/main.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use envelop_cli::cli::{Args, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Send { file, to } => handle_send_command(file, &to),
+    }
 }
+
+fn handle_send_command(file: PathBuf, to: &str) {}
