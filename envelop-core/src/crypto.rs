dd84f61d01da6e763eb42384929eee3e0b3a051a
Michal Pielka <mpielka726@gmail.com>
2026-03-27 20:50:26 +0100

    feat(core): add parse_recipients function

--- envelop-core/src/crypto.rs
+++ envelop-core/src/crypto.rs
@@ -1,7 +1,14 @@
+use age::ssh::{ParseRecipientKeyError, Recipient};
 pub fn encrypt(plaintext: &[u8], public_keys: &[&str]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     Ok(vec![])
 }
 
 pub fn decrypt(ciphertext: &[u8], private_key: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     Ok(vec![])
+}
+fn parse_recipients(public_keys: &[&str]) -> Result<Vec<Recipient>, ParseRecipientKeyError> {
+    public_keys
+        .iter()
+        .map(|&key| key.parse::<Recipient>())
+        .collect()
 }
