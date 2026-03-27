8df5a9db51382b77472a17694ad878cf3af645d9
Michal Pielka <mpielka726@gmail.com>
2026-03-27 20:31:11 +0100

    feat(core): add crypto function signatures

---
+++ envelop-core/src/crypto.rs
@@ -0,0 +1,7 @@
+pub fn encrypt(plaintext: &[u8], public_keys: &[&str]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    Ok(vec![])
+}
+
+pub fn decrypt(ciphertext: &[u8], private_key: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    Ok(vec![])
+}
