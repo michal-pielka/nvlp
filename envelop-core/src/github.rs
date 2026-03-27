15133703852ea4795dc3b547c819529a982804e2
Michal Pielka <mpielka726@gmail.com>
2026-03-27 22:12:45 +0100

    feat(core): error for status

--- envelop-core/src/github.rs
+++ envelop-core/src/github.rs
@@ -3,7 +3,9 @@ const BASE_URL: &str = "https://github.com";
 // TODO: error handling
 pub fn fetch_public_keys(username: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let public_keys_url = format!("{BASE_URL}/{username}.keys");
-    let text = reqwest::blocking::get(public_keys_url)?.text()?;
+    let text = reqwest::blocking::get(public_keys_url)?
+        .error_for_status()?
+        .text()?;
     let public_keys: Vec<String> = text
         .lines()
         .map(|s| s.to_string())
